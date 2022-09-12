53b58a70bc99d3667a5b0a94d8b184963f404165
aTfure <atfure@gmail.com>
2022-09-12 22:04:14 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end='')
         except:
             break
-    else:
-        count += 1
+        else:
+            count += 1
 
     print()
     return (count)
