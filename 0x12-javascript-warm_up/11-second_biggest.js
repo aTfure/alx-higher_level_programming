fd5d1cc7fecf04b0487977b2ae7f2eae05c14d87
aTfure <atfure@gmail.com>
2022-11-29 04:06:29 +0300

    [Fix]: using Math.max method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
 
-let biggest = 0;
-let i;
-const arrayNumbers = [];
-
-for (i = 2; i < process.argv.length; i++) {
-  if (Number.isNaN(parseInt(process.argv[i])) === false) {
-    arrayNumbers[i - 2] = parseInt(process.argv[i]);
-  }
+const len = process.argv.length;
+if (len < 4) {
+  console.log(0);
+} else {
+  const sortedArray = process.argv.sort((a, b) => a - b);
+  console.log(sortedArray[sortedArray.length - 2]);
 }
-
-if (arrayNumbers.length > 1) {
-  biggest = Math.max.apply(null, arrayNumbers);
-  i = arrayNumbers.indexOf(biggest);
-  arrayNumbers[i] = -Infinity;
-  biggest = Math.max.apply(null, arrayNumbers);
-}
-
-console.log(biggest);
