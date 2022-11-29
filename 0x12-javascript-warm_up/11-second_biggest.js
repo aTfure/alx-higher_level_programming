c6a3abb911a2673b276aa270d3ad778c44ced268
aTfure <atfure@gmail.com>
2022-11-29 04:03:41 +0300

    [Fix]: using Math.max method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@
 let biggest = 0;
 let i;
 const arrayNumbers = [];
+
 for (i = 2; i < process.argv.length; i++) {
   if (Number.isNaN(parseInt(process.argv[i])) === false) {
     arrayNumbers[i - 2] = parseInt(process.argv[i]);
