e94f0fa21fe64d255518002ae51d944f4dd41ccc
aTfure <atfure@gmail.com>
2022-11-29 01:44:29 +0000

    [Feature]: adding script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  console.log(parseInt(a) + parseInt(b));
+	return a + b;
 }
-add(process.argv[2], process.argv[3]);
+
+const a = +process.argv[2];
+const b = +process.argv[3];
+const c = add(a, b);
+console.log(c);
