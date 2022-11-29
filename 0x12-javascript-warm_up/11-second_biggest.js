9a929a6398d431c6152cd25d2602ecf04d5132fd
aTfure <atfure@gmail.com>
2022-11-29 04:33:49 +0300

    [Fix]: using Math.max method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
 const len = process.argv.length;
-
 if (len < 4) {
-
   console.log(0);
-
 } else {
-
   const sortedArray = process.argv.sort((a, b) => a - b);
-
   console.log(sortedArray[sortedArray.length - 2]);
-
 }
