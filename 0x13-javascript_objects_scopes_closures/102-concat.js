b4bbf05772e0e3290144a9b65ef3c5ca1adf2030
aTfure <atfure@gmail.com>
2022-11-30 00:25:52 +0000

    [Feature]: adding script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,15 +3,15 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-  fs.writeFile(process.argv[4], data, (err) => {
-    if (err) throw err;
-  });
+	if (err) throw err;
+	fs.writeFile(process.argv[4], data, (err) => {
+		if (err) throw err;
+	});
 });
 
 fs.readFile(process.argv[3], 'utf-8', (err, data) => {
-  if (err) throw err;
-  fs.appendFile(process.argv[4], data, (err) => {
-    if (err) throw err;
-  });
+	if (err) throw err;
+	fs.appendFile(process.argv[4], data, (err) => {
+		if (err) throw err;
+	});
 });
