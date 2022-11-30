a1190b1853c9e71f862ff7450e6e0f8554ed6e51
aTfure <atfure@gmail.com>
2022-11-30 00:18:35 +0000

    [Feature]: adding script that computes a dict using another dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const dict = require('./101-data').dict;
+const oldDict = require('./101-data').dict;
 const newDict = {};
-
-for (const key in dict) {
-  if (typeof (newDict[dict[key]]) === 'undefined') {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
+for (const k in oldDict) {
+	if (oldDict[k] in newDict) {
+		newDict[oldDict[k]].push(k);
+	} else {
+	newDict[oldDict[k]] = [k];
+	}
 }
 console.log(newDict);
