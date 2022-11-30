6b038245055834f10ac97a435e664987ae666d4f
aTfure <atfure@gmail.com>
2022-11-30 00:15:02 +0000

    [Feature]: adding script that imports an array and computes a new one

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const oldArray = require('./100-data').list;
 const newArray = oldArray.map(function (num, index) {
-  return num * index;
+	return num * index;
 });
 console.log(oldArray);
 console.log(newArray);
