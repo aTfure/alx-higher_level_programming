ee6e0b62365a541fc3e1779542f8ba0995fba30d
aTfure <atfure@gmail.com>
2022-12-04 23:04:11 +0300

    [Fix]: including case-sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N
-"""
+
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
-    states = cur.fetchall()
+def get_states(username, password, db_name):
+    """
+         lists all states with a name starting with N from the database
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
 
-    for state in states:
-        print(state)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states`\
+                   WHERE `name` REGEXP '^N' ORDER BY `id` ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        if ("N" in row[1]):
+            print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    get_states(sys.argv[1], sys.argv[2], sys.argv[3])
