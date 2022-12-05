c526b95fb08420d713956b33cdcdc9ae8c4f9b6f
aTfure <atfure@gmail.com>
2022-12-05 01:39:48 +0000

    [Feature]: creating modules of prev classes and a script that adds data

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,18 +10,18 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                pool_pre_ping=True)
+        Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    newState = State(name='California')
-    newCity = City(name='San Francisco')
-    newState.cities.append(newCity)
+        newState = State(name='California')
+        newCity = City(name='San Francisco')
+        newState.cities.append(newCity)
 
-    session.add(newState)
-    session.add(newCity)
-    session.commit()
+        session.add(newState)
+        session.add(newCity)
+        session.commit()
