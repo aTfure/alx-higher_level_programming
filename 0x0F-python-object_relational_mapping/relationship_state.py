3fee0f69fae51bb1c5519115ba041c86ee0ae547
aTfure <atfure@gmail.com>
2022-12-05 04:54:35 +0300

    [Feature]: creating modules of prev classes and a script that adds data

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -11,10 +11,10 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-        """
-        Class with id and name attributes of each state
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, unique=True, nullable=False, primary_key=True)
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="states")
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
