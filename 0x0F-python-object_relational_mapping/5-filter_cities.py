22e53440053dae2228a827c76af392a94cea8ad5
aTfure <atfure@gmail.com>
2022-12-05 05:07:59 +0300

    [Update]: injection free in task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,13 +6,13 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-        cur = db.cursor()
-        cur.execute("SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id \
-        WHERE states.name = '{}';".format(sys.argv[4]))
-        states = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-        print(", ".join([state[1] for state in states]))
+    print(", ".join([state[1] for state in states]))
