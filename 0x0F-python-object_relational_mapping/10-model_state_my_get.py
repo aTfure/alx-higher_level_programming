07e8ea02a9ccc7bbf0737a00d50bf42bdf384c86
aTfure <atfure@gmail.com>
2022-12-05 04:09:52 +0300

    [Feature]: adding a script that prints the State object

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,11 +9,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        state = session.query(State).filter(State.name == sys.argv[4]).first()
-        print("Not found" if not state else state.id)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
