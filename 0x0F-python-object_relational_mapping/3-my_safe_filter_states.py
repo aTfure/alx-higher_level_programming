2bde0f3f1fad6c976cfc25e8e2cab1d1ff45df7f
aTfure <atfure@gmail.com>
2022-12-05 05:10:45 +0300

    [Feature]: adding a script that lists all values of a state (safe)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,12 +7,12 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
-        states = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
-        for state in states:
-            print(state)
+    for state in states:
+        print(state)
