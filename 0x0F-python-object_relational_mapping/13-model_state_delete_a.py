0f9982fc0e66f74c41cdad3140108f92034f7a81
aTfure <atfure@gmail.com>
2022-12-05 01:22:29 +0000

    [Feature]: adding a script that deltes all State objects with a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,15 +9,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).all()
+        states = session.query(State).filter(State.name.like('%a%')).all()
 
-    for state in states:
-        session.delete(state)
+        for state in states:
+            session.delete(state)
 
-    session.commit()
+        session.commit()
