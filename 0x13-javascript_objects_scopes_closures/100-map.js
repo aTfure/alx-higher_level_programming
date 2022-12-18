69778c344039a5b214f4adc4eae57409d0db2030
aTfure <atfure@gmail.com>
2022-12-18 23:49:29 +0000

    changes

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const oldArray = require('./100-data').list;
-const newArray = oldArray.map(function (num, index) {
+const oldArray = require('.100-data').list;
+const newArray = oldArray.map(function(num, index){
 	return num * index;
 });
 console.log(oldArray);
